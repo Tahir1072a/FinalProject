f235d38c7e621bcd30e1e35e21b1876e0a85eea4
Tahir1072a <65045497+Tahir1072a@users.noreply.github.com>
2021-03-29 11:38:42 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // class : Referans tip olabilir demek, new() demek : new'lenebilir olmalı
+    // generic constraint yapmış olduk where ile
+    public interface IEntityRepository<T> where T: class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T Entity);
+        void Update(T Entity);
+        void Delete(T Entity);
+        List<T> GetAllByCategory(int categoryId);
+    }
+}
---
+++ DataAccess/Concrete/EntityFrameWork/EfCategoryDal.cs
@@ -0,0 +1,42 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFrameWork
+{
+    public class EfCategoryDal : ICategoryDal
+    {
+        public void Add(Category Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
